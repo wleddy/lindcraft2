c499cc9fd5ff14da632c29fc6c33ab9f929abcd1
Bill Leddy <bill@williesworkshop.net>
2018-11-28 20:36:30 -0500

    update contact fuction

--- www/views/home.py
+++ www/views/home.py
@@ -2,7 +2,7 @@ from flask import request, session, g, redirect, url_for, abort, \
      render_template, flash, Blueprint, Response
 from users.admin import login_required, table_access_required
 from takeabeltof.date_utils import datetime_as_string
-from takeabeltof.utils import render_markdown_for
+from takeabeltof.utils import render_markdown_for, handle_request_error
 from datetime import datetime
 from lindcraft.views import catalog
 
@@ -48,32 +48,72 @@ def about():
 @mod.route('/contact/', methods=['POST', 'GET',])
 def contact():
     setExits()
-    g.name = 'Contact Us'
+    g.title = 'Contact Us'
     from app import app
     from takeabeltof.mailer import send_message
     rendered_html = render_markdown_for(__file__,mod,'contact.md')
     show_form = True
     context = {}
+    success = True
+    mes = "No errors yet..."
     if request.form:
+        #import pdb;pdb.set_trace()
         if request.form['email'] and request.form['comment']:
             context.update({'date':datetime_as_string()})
             for key, value in request.form.items():
                 context.update({key:value})
-            send_message(
-                (app.config['CONTACT_NAME'],app.config['CONTACT_EMAIL_ADDR'],),
-                subject = "Contact from {}".format(app.config['SITE_NAME']),
-                html_template = "home/email/contact_email.html",
-                context = context,
-                reply_to = request.form['email'],
-            )
+                
+            # get best contact email
+            to = []
+            # See if the contact info is in Prefs
+            try:
+                from users.views.pref import get_contact_email
+                contact_to = get_contact_email()
+                if contact_to:
+                    to.append(contact_to)
+            except Exception as e:
+                printException("Need to update home.contact to find contacts in prefs.","error",e)
+                
+            try:
+                admin_to = None
+                if not to:
+                    to = [(app.config['CONTACT_NAME'],app.config['CONTACT_EMAIL_ADDR'],),]
+                if app.config['CC_ADMIN_ON_CONTACT']:
+                    admin_to = (app.config['MAIL_DEFAULT_SENDER'],app.config['MAIL_DEFAULT_ADDR'],)
+                    
+                if admin_to:
+                    to.append(admin_to,)
+                
+            except KeyError as e:
+                mes = "Could not get email addresses."
+                mes = printException(mes,"error",e)
+                if to:
+                    #we have at least a to address, so continue
+                    pass
+                else:
+                    success = False
+                    
+            if success:
+                # Ok so far... Try to send
+                success, mes = send_message(
+                                    to,
+                                    subject = "Contact from {}".format(app.config['SITE_NAME']),
+                                    html_template = "home/email/contact_email.html",
+                                    context = context,
+                                    reply_to = request.form['email'],
+                                )
         
             show_form = False
         else:
             context = request.form
             flash('You left some stuff out.')
             
-    
-    return render_template('contact.html',rendered_html=rendered_html, show_form=show_form, context=context)
+    if success:
+        return render_template('contact.html',rendered_html=rendered_html, show_form=show_form, context=context)
+            
+    handle_request_error(mes,request,500)
+    flash(mes)
+    return render_template('500.html'), 500
     
     
 @mod.route('/robots.txt', methods=['GET',])
